dd65752a9d833ca92d88afa74c8ad3e15db85cde
ana.scepanovic57@gmail.com <ana.scepanovic57@gmail.com>
2024-10-18 10:14:14 +0000

    Edited Pocetna

--- client_code/Pocetna/__init__.py
+++ client_code/Pocetna/__init__.py
@@ -21,6 +21,7 @@ class Pocetna(PocetnaTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     # Any code you write here will run when the form opens.
+
     self.init_components(**properties)
     custom_signup.login_flow.login_with_form()
     kor=anvil.users.get_user()['email']
@@ -46,8 +47,8 @@ class Pocetna(PocetnaTemplate):
     #print(self.worksheet.fields)
     #print(self.worksheet)
     
-
     
+
 
   def button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
